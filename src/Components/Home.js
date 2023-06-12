6cf77dea91320f58d926895cf45815846b629391
vijay <vijaykumar3198@gmail.com>
2023-06-12 11:16:46 +0530

    contactcontainer developed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Body from './Body'
-import Contact from './Contact'
+import Contact from './NewContact'
 import About from './About'
 import { Outlet } from 'react-router-dom'
 
